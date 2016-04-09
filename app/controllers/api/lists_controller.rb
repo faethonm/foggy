08b30a2ab2ab9a488a50d6d6594c509012fe90bd
Catalin Ionescu <catalin.ionescu282@gmail.com>
2016-04-09 12:20:50 -0700

    [api] Lists search endpoint done

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,10 +1,8 @@
-module Api
-  class ListsController < ApiController
-    def index
-      @lists = List.search params[:q]
-      respond_to do |format|
-        format.json { render @lists.to_json }
-      end
+class Api::ListsController < ApiController
+  def index
+    @lists = List.search params[:q]
+    respond_to do |format|
+      format.json { render json: @lists.to_json }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'contact' => 'home#contact'
+
+  namespace :api do
+    resources :lists, only: [:index]
+  end
 end
