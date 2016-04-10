9b31ae385eb4efb0265b22ab39beea926232d3e4
Catalin Ionescu <catalin.ionescu282@gmail.com>
2016-04-09 18:01:08 -0700

    [list] Make a list deleteable

--- app/models/list.rb
+++ app/models/list.rb
@@ -15,7 +15,7 @@
 
 class List < ActiveRecord::Base
   belongs_to :user
-  has_many  :list_items
+  has_many  :list_items, dependent: :destroy
   validates :name, presence: true
   before_save :downcase_name
 
